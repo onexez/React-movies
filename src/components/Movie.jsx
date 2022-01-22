d4c90aa5d479e4d54a277dceef0cb6ae12226d71
Dima <wowqet@mail.ru>
2022-01-22 19:41:48 +0300

    complete app

---
+++ src/components/Movie.jsx
@@ -0,0 +1,26 @@
+function Movie(props) {
+  const { Title: title, Year: year, imdbID: id, Type: type, Poster: poster } = props;
+
+  return (
+    <div className="card">
+      <div className="card-image waves-effect waves-block waves-light">
+        {poster === 'N/a' ? (
+          <img
+            className="activator"
+            src={`https://via.placeholder.com/300x450?text=${title}`}
+          ></img>
+        ) : (
+          <img className="activator" src={poster}></img>
+        )}
+      </div>
+      <div className="card-content">
+        <span className="card-title activator grey-text text-darken-4">{title}</span>
+        <p>
+          {year} <span className="right">{type}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export { Movie };
---
+++ src/components/Movies.jsx
@@ -0,0 +1,16 @@
+import { Movie } from './Movie';
+
+function Movies(props) {
+  const { movies = [] } = props;
+  return (
+    <div className="movies">
+      {movies.length ? (
+        movies.map((movie) => <Movie key={movie.imdbID} {...movie}></Movie>)
+      ) : (
+        <h4>Nothing found</h4>
+      )}
+    </div>
+  );
+}
+
+export { Movies };
